5d62e52f9689dcfe8acf22e14b690ce2fc51a427
Milana <138142677+meelana@users.noreply.github.com>
2024-05-07 17:11:09 +0300

    Fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("com.codeborne:selenide:7.2.1")
+    implementation("org.slf4j:slf4j-api")
+    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.20.0")
 }
 
 tasks.test {
